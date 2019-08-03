da25204b2e5bd4b78bd3749b843b3646b320b7fe
Adam Szalkowski <adam@szalkowski.de>
2019-08-03 09:28:39 +0000

    add kubernetes provider

--- provider.tf
+++ provider.tf
@@ -2,7 +2,15 @@ variable "client_secret" {}
 
 provider "azurerm" {
     version = "~>1.5"
-    client_secret = "${var.client_secret}"
+    client_secret = var.client_secret
+}
+
+provider "kubernetes" {
+    host = module.exposed_cluster.host
+
+    client_certificate     = base64decode(module.exposed_cluster.client_certificate)
+    client_key             = base64decode(module.exposed_cluster.client_key)
+    cluster_ca_certificate = base64decode(module.exposed_cluster.cluster_ca_certificate)
 }
 
 terraform {
